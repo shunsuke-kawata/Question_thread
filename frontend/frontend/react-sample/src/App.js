d2690e5fe4e3775ac081ea8412a783ef87064765
shunsuke kawata <20238120@edu.cc.saga-u.ac.jp>
2022-11-09 21:34:13 +0900

    add react router domを追加

--- frontend/frontend/react-sample/src/App.js
+++ frontend/frontend/react-sample/src/App.js
@@ -1,10 +1,15 @@
-import Main from "./Components/Main";
 import React from "react";
 import "./css/App.css";
+import Home from "./Pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   //アプリコンテスト用に質問掲示板を作成する
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Route path={`/`} element={<Home />} />
+    </BrowserRouter>
+  );
 }
 
 export default App;

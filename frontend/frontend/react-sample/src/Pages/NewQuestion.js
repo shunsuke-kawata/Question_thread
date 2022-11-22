7beb8b456caf60023440e0de1c16f767c8b2b3d8
shunsuke kawata <20238120@edu.cc.saga-u.ac.jp>
2022-11-23 04:34:00 +0900

    add formの型を作成

--- frontend/frontend/react-sample/src/Pages/NewQuestion.js
+++ frontend/frontend/react-sample/src/Pages/NewQuestion.js
@@ -1,5 +1,23 @@
+import { useForm } from "react-hook-form";
+import { Grid } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+
 const NewQuestion = () => {
-  return <p>新規質問追加画面</p>;
+  //データを登録するためのフックステート
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <>
+      <h1>新規質問追加画面</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>タイトル</label>
+        <input {...register("title")} />
+        <label>本文</label>
+        <input {...register("body")} />
+        <input type="submit" />
+      </form>
+    </>
+  );
 };
 
 export default NewQuestion;

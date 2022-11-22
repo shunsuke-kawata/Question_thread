229adc13d34a8cb32cfab2e15c19ead7f7dddaf1
shunsuke kawata <20238120@edu.cc.saga-u.ac.jp>
2022-11-22 23:58:59 +0900

    index 処理のひと段落

--- frontend/frontend/react-sample/src/Components/Question.jsx
+++ frontend/frontend/react-sample/src/Components/Question.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "../css/Question.css";
 
 const Question = ({ id, title, body, setDetailsFlag }) => {
+  const clickHandle = () => {
+    setDetailsFlag(true);
+    console.log(id, body, title);
+  };
   return (
-    <div className="question_body" onClick={() => setDetailsFlag(true)}>
+    <div className="question_body" onClick={() => clickHandle()}>
       <h4>{id}</h4>
       <p>{title}</p>
       <p>{body}</p>

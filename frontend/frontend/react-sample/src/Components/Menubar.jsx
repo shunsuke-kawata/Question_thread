b2742369620c002c19b4cc142b8fd34397405a85
shunsuke kawata <20238120@edu.cc.saga-u.ac.jp>
2022-11-24 03:10:18 +0900

    update リンクリストの配置を変更

--- frontend/frontend/react-sample/src/Components/Menubar.jsx
+++ frontend/frontend/react-sample/src/Components/Menubar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HStack } from "@mui/material";
 import "../css/Menubar.css";
 
 const Menubar = ({ setMenubarFlag }) => {
@@ -10,19 +11,16 @@ const Menubar = ({ setMenubarFlag }) => {
         <Link className="links" to="/">
           ホーム
         </Link>
-        <br />
         <Link className="links" to="/newQuestion">
           質問する
         </Link>
-        <br />
         <Link className="links" to="/signin">
           サインイン
         </Link>
-        <br />
         <Link className="links" to="/login">
           ログイン
         </Link>
-        <br />
+
         <button onClick={() => setMenubarFlag(false)}>閉じる</button>
       </div>
     </>

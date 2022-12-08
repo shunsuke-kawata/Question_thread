811edcd3c371d3d460b13717d97da38aa294c54d
shunsuke kawata <20238120@edu.cc.saga-u.ac.jp>
2022-12-09 06:55:47 +0900

    update routerの切り分け

---
+++ backend/go/router/router.go
@@ -0,0 +1,41 @@
+package router
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func createRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+	}))
+
+	router.POST("/post", func(c *gin.Context) {
+		/*
+		   DB操作など
+		*/
+		email := c.PostForm("email")
+		c.JSON(http.StatusCreated, gin.H{
+			"status": "ok",
+		})
+		fmt.Println(email)
+	})
+	return router
+}
